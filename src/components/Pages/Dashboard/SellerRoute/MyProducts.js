66c442cefbe4820ac0fa83f3db542c3d2ea519d1
jayed-phero <jayed.programminghero@gmail.com>
2022-11-26 15:06:03 +0600

    animation on advertiose section

--- src/components/Pages/Dashboard/SellerRoute/MyProducts.js
+++ src/components/Pages/Dashboard/SellerRoute/MyProducts.js
@@ -63,38 +63,46 @@ const MyProducts = () => {
                                             <th>{i + 1}</th>
                                             <td>{product.name}</td>
                                             <td>{product.reselPrice}</td>
-                                            <td>
-                                                <span className="badge badge-ghost badge-2xl">{product.role}</span>
+                                            <td className=''>
+                                                {
+                                                    product.role === 'advertised' ?
+                                                        (
+                                                            <span className="py-3 w-28 badge badge-ghost badge-2xl relative text-green-500 font-semibold"> {product.role}
+                                                                <span class="flex items-center justify-center h-3 w-3 absolute right-0 top-0">
+                                                                    <span class="animate-ping absolute inline-flex h-5 w-5 rounded-full bg-green-400 opacity-75"></span>
+                                                                    <span class="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+                                                                </span>
+                                                            </span>
+                                                        )
+
+                                                        :
+                                                        (
+                                                            <span className="py-3 w-28 badge badge-ghost badge-2xl relative text-red-500 font-semibold"> {product.role}
+                                                                <span class="flex items-center justify-center h-3 w-3 absolute right-0 top-0">
+                                                                    <span class="animate-ping absolute inline-flex h-5 w-5 rounded-full bg-red-400 opacity-75"></span>
+                                                                    <span class="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
+                                                                </span>
+                                                            </span>
+                                                        )
+                                                }
                                             </td>
                                             <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-                                                {product.role === 'adailable' ?
+                                                {product.role === 'available' ?
                                                     (
                                                         <span
                                                             onClick={() => makeAdvetise(product)}
-                                                            className='relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight'
+                                                            className='relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight inset-0 bg-green-200 rounded-full'
                                                         >
-                                                            <span
-                                                                aria-hidden='true'
-                                                                className='absolute inset-0 bg-green-200 opacity-50 rounded-full'
-                                                            ></span>
-                                                            <span className='relative'>
-                                                                Make Advertise
-                                                            </span>
+                                                            Make Advertise
                                                         </span>
                                                     )
                                                     :
                                                     (
                                                         <span
                                                             onClick={() => advertiseOff(product)}
-                                                            className='relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight'
+                                                            className='relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight bg-green-200  rounded-full'
                                                         >
-                                                            <span
-                                                                aria-hidden='true'
-                                                                className='absolute inset-0 bg-green-200 opacity-50 rounded-full'
-                                                            ></span>
-                                                            <span className='relative'>
-                                                                Advertise Close
-                                                            </span>
+                                                            Advertise Close
                                                         </span>
                                                     )
                                                 }
